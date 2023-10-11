1c45ac5ee5a7a5477de0d6491f14b34807f7c3ff
AhPro7 <ahpro001@gmail.com>
2023-10-11 10:39:15 +0300

    final edit

--- split_aud.py
+++ split_aud.py
@@ -9,20 +9,29 @@ import librosa.display
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
 
-audio_path = 'data/test-data/aud'
+audio_path = input('Enter the path of the audio files: ')
 extion = '.wav'
 # load the csv file
 path = input('Enter the path of the csv file: ')
+save_path = input('Enter the path to save the splitted audio files: ')
+#check if the save_path exists
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+    print('The save path does not exist, created a new one')
+hf_token = input('Enter the huggingface token: ')
+
 df = pd.read_csv(path)
 print(df.head())
 
 new_df = {
-    'video_pth': [],
 
+    'video_pth': [],
     'text': []
+
 }
 # check if the video_id values in path save in jo-aud folder by the name of video_id+chunk number.wav
-for i in range(len(df)):
+from tqdm import tqdm
+for i in tqdm(range(len(df))):
     video_id = df['video_id'][i]
     start_time = str(df['start'][i])
     end_time = str(df['end'][i])
@@ -43,7 +52,7 @@ for i in range(len(df)):
         audio_chunk = AudioSegment.from_wav(audio_file)
         audio_chunk = audio_chunk[start_time_in_ms:end_time_in_ms]
         # save the audio chunk
-        save_path = 'jo-aud'# need to make it dynamic
+        save_path = save_path # need to make it dynamic
         audio_chunk.export(os.path.join(save_path, video_id+'-'+str(i)+extion), format='wav')
         # add the video path to the new_df
         new_df['video_pth'].append(os.path.join(save_path, video_id+'-'+str(i)+extion))
@@ -57,3 +66,20 @@ new_df = pd.DataFrame(new_df)
 print(new_df.head())
 # save the new_df to csv file
 new_df.to_csv('splitted_jod.csv', index=False)
+
+# upload to huggingface
+
+from huggingface_hub.hf_api import HfFolder
+HfFolder.save_token(hf_token)
+
+from datasets import load_dataset
+from datasets import Dataset
+from datasets import Audio
+
+audio_dataset = Dataset.from_dict({
+  'audio': new_df['video_pth'],
+  'text':new_df['text']
+}).cast_column("audio", Audio())
+
+data_set_name = input('Enter the name of the dataset: ')
+audio_dataset.push_to_hub(data_set_name, use_auth_token=hf_token)
